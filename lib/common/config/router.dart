f53287a4e63645d912ae756c211fc835c5237020
Leonamin <yism810@gmail.com>
2023-07-26 12:12:22 +0900

    # EDIT - 페이지 빌더 분리

--- lib/common/config/router.dart
+++ lib/common/config/router.dart
@@ -1,8 +1,6 @@
-import 'dart:math';
-
-import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:shrew_kit/common/config/routes.dart';
+import 'package:shrew_kit/common/config/page_builder.dart';
 import 'package:shrew_kit/view/animated_list/test_animdated_list_view.dart';
 import 'package:shrew_kit/view/anime/animation_test.dart';
 import 'package:shrew_kit/view/concentric/concentric_view.dart';
@@ -40,47 +38,9 @@ final router = GoRouter(
               path: ShrewRoutes.cubeShrewView.path,
               name: ShrewRoutes.cubeShrewView.name,
               builder: (context, state) => const CubeShrewView(),
-              pageBuilder: (context, state) => CustomTransitionPage(
-                child: const CubeShrewView(),
-                transitionsBuilder:
-                    (context, animation, secondaryAnimation, child) {
-                  return Stack(
-                    children: <Widget>[
-                      SlideTransition(
-                        position: Tween<Offset>(
-                          begin: Offset.zero,
-                          end: const Offset(-1.0, 0.0),
-                        ).animate(animation),
-                        child: Container(
-                          color: Colors.white,
-                          child: Transform(
-                            transform: Matrix4.identity()
-                              ..setEntry(3, 2, 0.003)
-                              ..rotateY(pi / 2 * animation.value),
-                            alignment: FractionalOffset.centerRight,
-                            child: CubeHamsterView(),
-                          ),
-                        ),
-                      ),
-                      SlideTransition(
-                        position: Tween<Offset>(
-                          begin: const Offset(1.0, 0.0),
-                          end: Offset.zero,
-                        ).animate(animation),
-                        child: Container(
-                          color: Colors.white,
-                          child: Transform(
-                            transform: Matrix4.identity()
-                              ..setEntry(3, 2, 0.003)
-                              ..rotateY(pi / 2 * (animation.value - 1)),
-                            alignment: FractionalOffset.centerLeft,
-                            child: child,
-                          ),
-                        ),
-                      )
-                    ],
-                  );
-                },
+              pageBuilder: cubeTransitionPageBuilder(
+                const CubeHamsterView(),
+                const CubeShrewView(),
               ),
             ),
           ],
@@ -110,43 +70,3 @@ final router = GoRouter(
   ],
   debugLogDiagnostics: true,
 );
-
-// Widget cubeTransition(context, animation, secondaryAnimation, child) {
-//   var pi;
-//   return Stack(
-//     children: <Widget>[
-//       SlideTransition(
-//         position: Tween<Offset>(
-//           begin: Offset.zero,
-//           end: const Offset(-1.0, 0.0),
-//         ).animate(animation),
-//         child: Container(
-//           color: Colors.white,
-//           child: Transform(
-//             transform: Matrix4.identity()
-//               ..setEntry(3, 2, 0.003)
-//               ..rotateY(pi / 2 * animation.value),
-//             alignment: FractionalOffset.centerRight,
-//             child: exitPage,
-//           ),
-//         ),
-//       ),
-//       SlideTransition(
-//         position: Tween<Offset>(
-//           begin: const Offset(1.0, 0.0),
-//           end: Offset.zero,
-//         ).animate(animation),
-//         child: Container(
-//           color: Colors.white,
-//           child: Transform(
-//             transform: Matrix4.identity()
-//               ..setEntry(3, 2, 0.003)
-//               ..rotateY(pi / 2 * (animation.value - 1)),
-//             alignment: FractionalOffset.centerLeft,
-//             child: enterPage,
-//           ),
-//         ),
-//       )
-//     ],
-//   );
-// }
