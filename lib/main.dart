683cb8f2d0b2a349d63993a37ebcebf0c5d41b3a
Leonamin <yism810@gmail.com>
2023-07-31 18:51:23 +0900

    # FIX - intl initialize

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:logger/logger.dart';
 import 'package:shrew_kit/common/config/router.dart';
 
 final logger = Logger();
 
-void main() => runApp(const MyApp());
+void main() {
+  initializeDateFormatting();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
