2ee7d8c7a178335b556cef9e5e923db394348a1d
Leonamin <yism810@gmail.com>
2023-08-01 11:24:24 +0900

    # ADD - calendar_day_view 일정 계단 현상

--- lib/view/scheduling/libs/lib_calendar_day_view.dart
+++ lib/view/scheduling/libs/lib_calendar_day_view.dart
@@ -1,15 +1,42 @@
 import 'package:calendar_day_view/calendar_day_view.dart';
 import 'package:flutter/material.dart';
+import 'package:shrew_kit/util/date_time_ext.dart';
 
-class LibCalendarDayView extends StatelessWidget {
+class LibCalendarDayView extends StatefulWidget {
   const LibCalendarDayView({super.key});
+
+  @override
+  State<LibCalendarDayView> createState() => _LibCalendarDayViewState();
+}
+
+class _LibCalendarDayViewState extends State<LibCalendarDayView> {
+  final _todayStart = DateTimeExt.getToday();
+
+  List<DateTime> _testDateTime = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    for (var i = 0; i < 22; i++) {
+      _testDateTime.add(_todayStart.add(Duration(minutes: i * 30)));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: OverFlowCalendarDayView<String>(
-        events: [],
+        events: [
+          for (var i = 0; i < _testDateTime.length; i++)
+            DayEvent(
+              start: TimeOfDay.fromDateTime(_testDateTime[i]),
+              end: TimeOfDay.fromDateTime(
+                  _testDateTime[i].add(const Duration(hours: 1))),
+              value: 'Event $i',
+            ),
+        ],
         dividerColor: Colors.black,
         startOfDay: const TimeOfDay(hour: 00, minute: 0),
         endOfDay: const TimeOfDay(hour: 23, minute: 0),
